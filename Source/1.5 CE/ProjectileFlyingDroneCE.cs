8d08fcc8c218d5c3cc46414919e5a5513e75a82d
zhuang jr <1095313400@qq.com>
2024-07-07 18:14:10 +0800

    fix: drone rendering

--- Source/1.5 CE/ProjectileFlyingDroneCE.cs
+++ Source/1.5 CE/ProjectileFlyingDroneCE.cs
@@ -54,7 +54,7 @@ namespace PLA.CE
             base.Impact(hitThing);
         }
 
-        public override void Draw()
+        public override void DrawAt(Vector3 drawLoc, bool flip = false)
         {
             Vector3 offset = new Vector3(0, 0, 2);
             offset.z += (float)Math.Sin((float)this.ticksToImpact / 12f) * 0.1f;
@@ -66,8 +66,22 @@ namespace PLA.CE
             Graphics.DrawMesh(MeshPool.GridPlane(this.def.graphicData.drawSize), position + offset, Quaternion.identity, this.def.DrawMatSingle, 0);
 
             base.Comps_PostDraw();
-
         }
+
+        // public override void Draw()
+        // {
+        //     Vector3 offset = new Vector3(0, 0, 2);
+        //     offset.z += (float)Math.Sin((float)this.ticksToImpact / 12f) * 0.1f;
+        //     Vector3 position = new Vector3(this.ExactPosition.x, this.def.Altitude - 0.01f, this.ExactPosition.z - Mathf.Lerp(this.shotHeight, 0f, this.FlightTicks / this.startingTicksToImpact));
+        //     if (this.def.projectile.shadowSize > 0f)
+        //     {
+        //         Graphics.DrawMesh(MeshPool.GridPlane(this.def.graphicData.drawSize), position, this.ExactRotation, ProjectileFlyingDroneCE.CustomShadowMaterial, 0);
+        //     }
+        //     Graphics.DrawMesh(MeshPool.GridPlane(this.def.graphicData.drawSize), position + offset, Quaternion.identity, this.def.DrawMatSingle, 0);
+
+        //     base.Comps_PostDraw();
+
+        // }
 
         private static readonly Material CustomShadowMaterial = MaterialPool.MatFrom("Things/Skyfaller/SkyfallerShadowCircle", ShaderDatabase.Transparent);
 	}
