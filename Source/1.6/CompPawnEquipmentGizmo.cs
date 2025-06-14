464547a9f723c8f4f1ca9677e9798161aa017199
IssacZhuang <1095313400@qq.com>
2025-06-14 14:26:19 +0800

    1.6

---
+++ Source/1.6/CompPawnEquipmentGizmo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using RimWorld;
+using Verse;
+using System.Collections.Generic;
+
+namespace PLA
+{
+	class CompPawnEquipmentGizmo : ThingComp
+	{
+		public override IEnumerable<Gizmo> CompGetGizmosExtra()
+		{
+			Pawn pawn = this.parent as Pawn;
+			ThingWithComps equip = (pawn != null) ? pawn.equipment.Primary : null;
+			if (equip != null && !equip.AllComps.NullOrEmpty<ThingComp>())
+			{
+				foreach (ThingComp comp in equip.AllComps)
+				{
+					CompSecondaryVerb scondaeryVerb = comp as CompSecondaryVerb;
+					if (scondaeryVerb == null)
+					{
+						continue;
+					}
+					foreach (Gizmo gizmo in scondaeryVerb.CompGetGizmosExtra())
+					{
+						yield return gizmo;
+					}
+					yield break;
+				}
+			}
+			yield break;
+		}
+	}
+}
